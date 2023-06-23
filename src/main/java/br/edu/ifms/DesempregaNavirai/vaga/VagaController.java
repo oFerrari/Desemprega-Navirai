e63b1b1d604c79016caf2c24aad0d960e95fcfc5
Andrei Ferrari Domingos <andrei.domingos@estudante.ifms.edu.br>
2023-06-22 20:01:17 -0400

    Terminando Projeto

--- src/main/java/br/edu/ifms/DesempregaNavirai/vaga/VagaController.java
+++ src/main/java/br/edu/ifms/DesempregaNavirai/vaga/VagaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/candidato")
+@RequestMapping("/api/vaga")
 public class VagaController {
 
     @Autowired // faz o Spring criar uma instância de DiciplinaService
